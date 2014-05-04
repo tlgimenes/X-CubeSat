7d05134f0d997d702c34473457258c24d3d494a6
tlgimenes <tlgimenes@gmail.com>
2014-05-05 00:28:08 +0200

    improoved bug messages and the program name

--- dataBase/tables.sql
+++ dataBase/tables.sql
@@ -1,24 +1,24 @@
-CREATE TABLE IF NOT EXISTS XCubeSat_Controler_session(
+CREATE TABLE IF NOT EXISTS XCubeSat_Controller_session(
     SatId integer primary key autoincrement not null,
     SatName text not null,
     AmountOfScripts integer not null,
     ScriptNames text);
 
-CREATE TABLE IF NOT EXISTS XCubeSat_Controler_scripts(
+CREATE TABLE IF NOT EXISTS XCubeSat_Controller_scripts(
     ScriptId integer primary key autoincrement not null, 
     ScriptContent text not null,
     Alias text not null,
     ScriptName text not null,
     unique(ScriptName));
 
-CREATE TABLE IF NOT EXISTS XCubeSat_Controler_sciData(
+CREATE TABLE IF NOT EXISTS XCubeSat_Controller_sciData(
     DataId integer primary key autoincrement not null,
     SatName text not null,
     TimeStamp integer not null, 
     RawContent text not null, 
     DataNickName text not null);
 
-CREATE TABLE IF NOT EXISTS XCubeSat_Controler_wodData(
+CREATE TABLE IF NOT EXISTS XCubeSat_Controller_wodData(
     DataId integer primary key autoincrement not null,
     SatName text not null,
     TimeStamp integer not null,
